7d3a39deabbd9cb2b1cc9ab17b63395054a1cc39
Katherine Shiro <katie.shiro@gmail.com>
2017-01-06 08:57:26 -0600

    adds bidding functionality

--- great-bay.js
+++ great-bay.js
@@ -1,6 +1,8 @@
+//npm packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+//database connection info
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,11 +11,12 @@ var connection = mysql.createConnection({
     database: "greatbay_db"
 });
 
+//connects to database
 connection.connect(function(err) {
     if (err) throw err;
-    // console.log("connected as id " + connection.threadId);
 });
 
+//initializes app
 initialize();
 
 //FUNCTIONS
@@ -27,10 +30,7 @@ function initialize() {
         if (answers.action === "POST AN ITEM") {
             postItem();
         } else if (answers.action === "BID ON AN ITEM") {
-            // bidOnItem();
-            console.log("bid");
-        } else {
-            console.log("Please enter 'POST AN ITEM' or 'BID ON AN ITEM'.");
+            bidOnItem();
         }
     });
 }
@@ -58,6 +58,50 @@ function postItem() {
                 initialize();
             }
 
+        });
+    });
+}
+
+function bidOnItem() {
+    connection.query("SELECT item FROM bids", function(err, res) {
+        if (err) throw err;
+        var itemsArr = [];
+        for (var i = 0; i < res.length; i++) {
+            itemsArr.push(res[i].item);
+        }
+        inquirer.prompt([{
+            name: "item",
+            message: "Select an item to bid on.",
+            type: "list",
+            choices: itemsArr
+        }]).then(function(answers) {
+            var selectedItem = answers.item;
+            inquirer.prompt([{
+                name: "bidder",
+                message: "Enter your name."
+            }, {
+                name: "bid",
+                message: "Enter your bid."
+            }]).then(function(answers) {
+                connection.query("SELECT bid FROM bids WHERE item=\'" + selectedItem + "\'", function(err, res) {
+                    if (err) throw err;
+                    if (res[0].bid < answers.bid) {
+                        connection.query("UPDATE bids SET ? WHERE ?", [{
+                            high_bidder: answers.bidder,
+                            bid: answers.bid
+                        }, {
+                            item: selectedItem
+                        }], function(err, res) {
+                            if (err) throw err;
+                            console.log("Congrats, you are now the highest bidder for " + selectedItem + " with a bid of " + answers.bid);
+                            initialize();
+                        });
+                    } else {
+                        console.log("Sorry, that bid is not high enough.");
+                        initialize();
+                    }
+                });
+            });
         });
     });
 }
